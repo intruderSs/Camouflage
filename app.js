672afb9f7071c0301977d868da9fdd3746157ff0
Shahil Sinha <shahilsinha1133@gmail.com>
2021-12-05 14:05:32 +0530

    Simple Login with Email and PAsswprd using Mongoose

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 const express = require("express");
 const ejs = require("ejs");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const app = express();
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended : true}));
+
+mongoose.connect("mongodb://localhost:27017/camDB", {useNewUrlParser: true});
+
+const userSchema = ({
+  email: String,
+  password: String
+});
+
+const User = new mongoose.model("User", userSchema);
+
 
 app.get("/", function(req, res) {
   res.render("home");
@@ -17,12 +31,36 @@ app.get("/register", function(req, res) {
   res.render("register");
 });
 
-app.get("/camouflage", function(req, res) {
-  res.render("camouflage");
+app.post("/register", function(req, res) {
+  const newUser = new User({
+    email: req.body.username,
+    password: req.body.password
+  });
+  newUser.save(function(err) {
+    if (!err){
+      res.render("camouflage");
+    }else{
+      console.log(err);
+    }
+  });
 });
 
-app.get("/submit", function(req, res) {
-  res.render("submit");
+app.post("/login", function(req, res) {
+  const username = req.body.username;
+  const password = req.body.password;
+
+User.findOne({email: username}, function(err, foundUser) {
+  if (err) {
+    console.log(err);
+  }else{
+    if (foundUser) {
+      if(foundUser.password === password) {
+        res.render("camouflage");
+      }
+    }
+  }
+});
+
 });
 
 app.listen(3000, function() {
